124991cff120065d264fc6c1bbad38376d46d4f7
sankaranarayanankj <53070371+sankaranarayanankj@users.noreply.github.com>
2019-07-19 09:20:03 +0530

    Add files via upload

---
+++ python/armstrong.py
@@ -0,0 +1,6 @@
+x=int(input("Enter the number:"))
+n=0
+while(x!=0):
+    n=n+1
+    x=x/10
+print(n)
---
+++ python/palindrome.py
@@ -0,0 +1,7 @@
+n=input("Enter String:")
+n1=n[::-1]
+print(n1)
+if(n1==n):
+    print("Palindrome")
+else:
+    print("Not palindrome")
