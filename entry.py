1715bc4ac4c67c2071b4acc79e2765ab442357f7
sankaranarayanankj <53070371+sankaranarayanankj@users.noreply.github.com>
2019-07-31 09:50:41 +0530

    Add files via upload

--- entry.py
+++ entry.py
@@ -1,13 +1,45 @@
-n=int(input("Enter the entries:"))
-t=[]
+n=int(input("Enter Entries:"))
 l=[]
+l1=[]
+q=[]
+l2=[]
+p=[]
+count=0
 for i in range(0,n):
-    x=input("Enter the Time period,entries,entry/exit:")
-    l.append(x.split(","))
-t.append(l)
-print(t)
+    x=input("Enter entry->")
+    x=x.split(",")
+    l.append(x)
+#print(l[1][0])
 for i in range(0,n):
-    if(t[i][2]='entry'):
-        income+=t[i][1]
+    x=int(l[i][0])
+    y=int(l[i][1])
+    z=str(l[i][2])
+    l1.append(x)
+    l1.append(y)
+    l1.append(z)
+    q.append(l1)
+    l1=[]
+#print(q)
+#print(l1)
+for i in range(0,len(q)):
+    if(q[i][2]=='entry'):
+        count+=q[i][1]
     else:
-        income-=t[i][l]
+        count-=q[i][1]
+    l2.append(count)
+print(l2)
+w=max(l2)
+if(l2[-1]!=0):
+    print("This time period is invalid")
+else:
+    for i in range(0,len(l2)):
+        if(w==l2[i]):
+            print(i)
+            break
+for i in range(0,i,-1):
+    if(q[i][2]=='Entry'):
+        p.append(q[i][0])
+print(p)
+
+          
+       
